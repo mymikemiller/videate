0b1752d65f5637720c619d8bc55865eae9a0b35a
Michael J. Miller <mikem.exe@gmail.com>
2020-09-17 22:00:16 -0600

    support multiple uri formatters (#26)

--- vidcast/bin/feed_formatters/feed_formatter.dart
+++ vidcast/bin/feed_formatters/feed_formatter.dart
@@ -3,13 +3,18 @@ import 'package:vidlib/vidlib.dart';
 typedef Uri UriTransformer(Uri input);
 
 abstract class FeedFormatter<T> {
-  final UriTransformer uriTransformer;
+  final List<UriTransformer> uriTransformers;
 
   T format(Feed feed);
 
-  FeedFormatter([this.uriTransformer]);
+  FeedFormatter([this.uriTransformers]);
 
   Uri transformUri(Uri input) {
-    return uriTransformer == null ? input : uriTransformer(input);
+    if (uriTransformers != null) {
+      // Run all the transformers in order
+      return uriTransformers.fold(
+          input, (previousValue, transformer) => transformer(previousValue));
+    }
+    return input;
   }
 }

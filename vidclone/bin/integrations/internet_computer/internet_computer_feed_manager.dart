7f73328d4ecf9de65b8fe07728a1ba9c89bf8089
Michael J. Miller <mikem.exe@gmail.com>
2021-03-24 22:07:13 -0600

    Write to Internet Computer

--- vidclone/bin/integrations/internet_computer/internet_computer_feed_manager.dart
+++ vidclone/bin/integrations/internet_computer/internet_computer_feed_manager.dart
@@ -3,7 +3,11 @@ import '../../feed_manager.dart';
 
 // Manages a feed running on dfinity's Internet Computer (https://dfinity.org/)
 class InternetComputerFeedManager extends FeedManager {
-  String _feedName;
+  String feedKey;
+
+  // True to access a locally running IC instance, False to use canisters on
+  // the IC network
+  bool localIC;
 
   // The working directory from which to run the dfx commands (i.e. the
   // directory containing a dfx.json file)
@@ -13,19 +17,28 @@ class InternetComputerFeedManager extends FeedManager {
   String get id => 'internet_computer';
 
   @override
-  String get feedName => _feedName;
+  String get feedName => feedKey;
 
   @override
   void configure(ClonerTaskArgs feedManagerArgs) {
-    _feedName = feedManagerArgs.get('feedName');
+    feedKey = feedManagerArgs.get('feedName');
+    localIC = feedManagerArgs.get('localIC').toLowerCase() == 'true';
     dfxWorkingDirectory = feedManagerArgs.get('dfxWorkingDirectory');
   }
 
   @override
   Future<bool> populate() async {
-    final output = await processRunner(
-        'dfx', ['canister', 'call', 'credits', 'getFeed', feedName],
-        workingDirectory: dfxWorkingDirectory);
+    final List<String> network = localIC ? [] : ['--network', 'ic'];
+    final args = [
+      'canister',
+      ...network,
+      'call',
+      'credits',
+      'getFeed',
+      '("$feedName")'
+    ];
+    final output =
+        await processRunner('dfx', args, workingDirectory: dfxWorkingDirectory);
 
     final stdout = output.stdout;
 
@@ -34,10 +47,69 @@ class InternetComputerFeedManager extends FeedManager {
       return false;
     }
 
-    print(output);
     return false;
   }
 
   @override
-  Future<void> write() async {}
+  Future<void> write() async {
+    final feedCandidString = toCandidString(feed);
+
+    final List<String> network = localIC ? [] : ['--network', 'ic'];
+    final args = [
+      'canister',
+      ...network,
+      'call',
+      'serve',
+      'addFeed',
+      '("$feedName", $feedCandidString)'
+    ];
+    final output =
+        await processRunner('dfx', args, workingDirectory: dfxWorkingDirectory);
+
+    final stderr = output.stderr;
+    if (stderr.isNotEmpty) {
+      throw stderr;
+    }
+
+    final stdout = output.stdout;
+
+    if (stdout == '(null)\n)') {
+      // No feed found with the given name
+      return false;
+    }
+
+    return true;
+  }
+
+  String toCandidString(Feed feed) {
+    final mediaListCandid = feed.mediaList.map((servedMedia) => '''
+    record { 
+      uri="${servedMedia.uri}"; 
+      title="${servedMedia.media.title}"; 
+      source=record { 
+        id="${servedMedia.media.source.id}"; 
+        uri="${servedMedia.media.source.uri}"; 
+        platform=record { 
+          id="${servedMedia.media.source.platform.id}"; 
+          uri="${servedMedia.media.source.platform.uri}"
+        } 
+      }; 
+      description="${servedMedia.media.description}"
+    }''').join('; ');
+
+    final candid = '''
+record { 
+  title="${feed.title}"; 
+  link="http://example.com"; 
+  description="${feed.description}"; 
+  email="${feed.email}"; 
+  author="${feed.author}"; 
+  imageUrl="${feed.imageUrl}"; 
+  mediaList=vec { 
+$mediaListCandid
+  }; 
+  subtitle="Powered by Videate" 
+}''';
+    return candid;
+  }
 }

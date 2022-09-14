b9c05c01e9392aab3e52234d11bf5394fabf4d5c
Michael J. Miller <mikem.exe@gmail.com>
2022-09-14 10:59:51 -0600

    properly log out

--- credits/src/frontend/src/hooks.ts
+++ credits/src/frontend/src/hooks.ts
@@ -43,7 +43,7 @@ export function useAuthClient(props?: UseAuthClientProps) {
   const logout = () => {
     setIsAuthenticated(false);
     setActor(undefined);
-    navigate('/');
+    authClient?.logout({ returnTo: "/" });
   };
 
   useEffect(() => {

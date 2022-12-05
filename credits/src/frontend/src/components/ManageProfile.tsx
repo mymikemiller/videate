50a7e63ccb7042ba6f4c71bc2ce9ced817b2946d
Michael J. Miller <mikem.exe@gmail.com>
2022-12-04 21:05:56 -0700

    Distribution simulation display

--- credits/src/frontend/src/components/ManageProfile.tsx
+++ credits/src/frontend/src/components/ManageProfile.tsx
@@ -7,7 +7,7 @@ import {
 import Cancel from "@spectrum-icons/workflow/Cancel";
 import Delete from "@spectrum-icons/workflow/Delete";
 import Edit from "@spectrum-icons/workflow/Edit";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -26,8 +26,43 @@ import { _SERVICE as _SERVE_SERVICE } from "../../../declarations/serve/serve.di
 
 function ManageProfile() {
   const [isEditing, setIsEditing] = useState(false);
+  const [distributionSimAmount, setDistributionSimAmount] = useState(0.0);
+  const [distributionSimElement, setDistributionSimElement] = useState(<></>);
   const { actor, profile, setProfile } = useContext(AppContext);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (distributionSimAmount == 0.0) {
+      setDistributionSimElement(<></>);
+      return;
+    }
+
+    (async () => {
+      const distributionAmounts = await actor!.getDistribution(distributionSimAmount);
+
+      const listElements = [];
+      for (var principalAndAmount of distributionAmounts) {
+        const principal = principalAndAmount[0];
+        const nameResult = await actor!.getContributorName(principal);
+        const name = nameResult.length == 1 ? nameResult[0] : "Contributor not found";
+        console.log(name);
+        const amountAsString = (Math.round(principalAndAmount[1] * 100) / 100).toFixed(2);
+        listElements.push(
+          <li style={{ listStyleType: 'none', marginBottom: '1em' }} >
+            <Text>${amountAsString}: {name}</Text>
+          </li>
+        );
+      };
+
+      const element =
+        <ul style={{ padding: 0 }} >
+          {listElements}
+        </ul>
+
+      setDistributionSimElement(<div>{element}</div>);
+    })();
+
+  }, [distributionSimAmount]);
 
   const deleteProfile = async () => {
     if (
@@ -105,6 +140,12 @@ function ManageProfile() {
               )
             })}
           </ul>
+          <div>
+            <ActionButton onPress={() => setDistributionSimAmount(10.0)}>
+              <Text>Simulate $10 Gift</Text>
+            </ActionButton>
+          </div>
+          {distributionSimElement}
           <ButtonGroup>
             <ActionButton onPress={() => setIsEditing(true)}>
               <Edit />
